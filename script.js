ca2a7ccbfcae6e0c6ff2e316914d6a6f55e338ac
henry <63601449+HenryBass@users.noreply.github.com>
2022-04-11 17:34:29 +0000

    idk

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-page = "Among Us"
+pages = ["Among Us", "Frog", "Stack Overflow", "Feynman Diagram", "Reddit", "Taiwan"]
+
+var r = Math.floor(Math.random() * pages.length);
+console.log(r)
+page = pages[r]
 
 const request = new Request(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${page.replace(" ", "%20")}&origin=*&indexpageids`);
 
-var summary = ""
-var title = ""
+var summary
+var title
+var id
+var casetitle
+var redacted
+var len
 
 fetch(request).then(response => {
   response.json().then(data => {
-  var id = data.query.pageids;
-  var summary = (data.query.pages[id].extract)
-  var title = (data.query.pages[id].title)
-  var redacted = summary.replaceAll(title, "&#129001".repeat(title.length))
-  
-  document.getElementById("summary").innerHTML = redacted;
+  id = data.query.pageids;
+  summary = (data.query.pages[id].extract)
+  title = (data.query.pages[id].title)
+  updatetext()
 });
 })
 
+function updatetext() {
+  casetitle = new RegExp(title, 'ig');
+  redacted = summary.replaceAll(casetitle, "&#129001".repeat(title.length))
+
+  document.getElementById("summary").innerHTML = redacted.slice(0, 300) + "...";
+}
+
+document.getElementById("input").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    document.getElementById("")
+  }
+})
