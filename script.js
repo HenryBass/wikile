a8890afd5467b7ed1be08e491b0e426d760af466
henry <63601449+HenryBass@users.noreply.github.com>
2022-04-12 20:31:51 +0000

    g

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-pages = ["Stack Overflow", "Feynman Diagram", "Taiwan", "Apollo 5", "Manhattan Project"]
+//import Cookies from 'js.cookie.mjs'
+
+pages = ["Hack Club", "Feynman Diagram", "Unicode", "Taiwan", "Malbolge", "Manhattan Project", "Brainfuck", "Apollo 5", "Axiom", "Soyuz", "Radon", "Stack Overflow", "Watt", "The Great Dictator", "Congo River", "Falcon Heavy", "Lexicon", "League of Nations", "Processor register", "Cryptography"]
 
 attempts = 0;
 
-var r = Math.floor(Math.random() * pages.length);
+var timestamp = Math.floor((Date.now() / 8.64e7));
 
-page = pages[r]
+var day = (timestamp - 19093)
+
+var page = pages[day % pages.length - 1]
+
+document.getElementById("title").innerHTML = "Wikile #" + day;
 
 const request = new Request(`https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${page.replace(" ", "%20")}&origin=*&indexpageids`);
 
@@ -18,6 +24,7 @@ var lastword
 var word = []
 var letters = 0
 var done = false;
+var results = "Wikile #" + day + "<br>";
 
 fetch(request).then(response => {
   response.json().then(data => {
@@ -36,7 +43,11 @@ function updatetext() {
   casetitle = new RegExp(title, 'gi');
   redacted = summary.replaceAll(casetitle, word.join(""))
 
-  document.getElementById("summary").innerHTML = "\"" + redacted.slice(0, 200 * (attempts + 1)) + "..." + "\"";
+  if (attempts == 3) {
+  document.getElementById("summary").innerHTML = redacted.slice(0, 200 * (attempts + 1));
+  } else {
+      document.getElementById("summary").innerHTML = redacted.slice(0, 200 * (attempts + 1))  + "<br>...";
+  }
 }
 
 document.getElementById("input").addEventListener("keypress", function (event) {
@@ -45,29 +56,42 @@ document.getElementById("input").addEventListener("keypress", function (event) {
   
   if (event.key === "Enter" && document.getElementById("input").value != "" && done == false) {
     var prediction = document.getElementById("input").value.toLowerCase();
+
     for (var i = 0; i < title.length; i++) {
       if (prediction[i] === title[i].toLowerCase()) {
         word[i] = title[i];
         letters += 1
+        results += "&#129001";
+      } else if (prediction.includes(title[i])){
+        word[i] = "🟨";
+        results += "🟨";
+      }
+        else {
+        results += "&#11035";
       }
     }
-
+    
+    
+    results += "<br>"
     if (letters < title.length && attempts == 2) {
       message = "<br> <p style=\"font-size: 16px\">You lose! The title was: " + title + "</p>"
       done = true;
+      document.getElementById("title").innerHTML = results;
     } else if (letters == title.length) {
       message = "<br> <p style=\"font-size: 16px\">You win! The title was: " + title + "</p>"
       done = true;
+      document.getElementById("title").innerHTML = results;
     }
     else {
-      message = "<br> <p style=\"font-size: 10px\">Correct Letters: " + letters + "</p>"
+      message = "<br> <p style=\"font-size: 10px\">Correct Letters: " + letters + "/" + title.length + "</p>"
     }
     
     
-    document.getElementById("out").innerHTML += "<br>" + document.getElementById("input").value.toUpperCase() + message;
+    document.getElementById("out").innerHTML += "<br>" +     document.getElementById("input").value.toUpperCase() + message;
     document.getElementById("input").value = "";
     attempts += 1;
     document.getElementById("remaining").innerHTML = "Remaining Attempts: " + (3 - attempts);
+    
     updatetext()
     letters = 0;
   }
