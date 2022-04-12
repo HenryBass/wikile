e8e90f75a32afddfb84086f2856b78664e2b5cdc
henry <63601449+HenryBass@users.noreply.github.com>
2022-04-12 01:21:37 +0000

    b

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-pages = ["Among Us", "Frog", "Stack Overflow", "Feynman Diagram", "Reddit", "Taiwan"]
+pages = ["Among Us", "Frog", "Stack Overflow", "Feynman Diagram", "Reddit", "Taiwan", "Hack Club", "Apollo 5"]
 
-attempts = 5;
+attempts = 0;
 
 var r = Math.floor(Math.random() * pages.length);
 
@@ -14,29 +14,60 @@ var id
 var casetitle
 var redacted
 var len
+var lastword
+var word = []
+var letters = 0
 
 fetch(request).then(response => {
   response.json().then(data => {
   id = data.query.pageids;
   summary = (data.query.pages[id].extract)
   title = (data.query.pages[id].title)
+    for(var i=0;i<title.length;i++) {
+      word[i] = "&#129001"
+    }
+  lastword = title
   updatetext()
 });
 })
 
 function updatetext() {
-  casetitle = new RegExp(title, 'ig');
-  redacted = summary.replaceAll(casetitle, "&#129001".repeat(title.length))
+  casetitle = new RegExp(title, 'gi');
+  redacted = summary.replaceAll(casetitle, word.join(""))
 
-  document.getElementById("summary").innerHTML = redacted.slice(0, 300) + "...";
+  document.getElementById("summary").innerHTML = redacted.slice(0, 200 * (attempts + 1)) + "...";
 }
 
 document.getElementById("input").addEventListener("keypress", function (event) {
-  if (attempts >= 0) {
-  if (event.key === "Enter") {
-    document.getElementById("out").innerHTML += "<br>" + document.getElementById("input").value;
+  
+  if (attempts < 3) {
+  
+  if (event.key === "Enter" && document.getElementById("input").value != "") {
+    var prediction = document.getElementById("input").value.toLowerCase();
+    for (var i = 0; i < title.length; i++) {
+      if (prediction[i] === title[i].toLowerCase()) {
+        word[i] = title[i];
+        letters += 1
+      }
+    }
+
+    if (letters < title.length && attempts == 2) {
+      message = "<br> <p style=\"font-size: 16px\">You lose! The title was: " + title + "</p>"
+    } else if (letters == title.length) {
+      message = "<br> <p style=\"font-size: 16px\">You win! The title was: " + title + "</p>"
+    }
+    else {
+      message = "<br> <p style=\"font-size: 10px\">Correct Letters: " + letters + "</p>"
+    }
+    
+    
+    document.getElementById("out").innerHTML += "<br>" + document.getElementById("input").value.toUpperCase() + message;
     document.getElementById("input").value = "";
+    attempts += 1;
+    updatetext()
+    letters = 0;
   }
-    attempts -= 1;
+    
   }
-})
+
+});
